14ece007732203c5f515d526cd425ab251b7210f
Hendrik Lammers <lammers@gmail.com>
2019-03-25 21:06:29 +0700

    Use cheerio to grab data usage from page

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import https from 'https'
+import cheerio from 'cheerio'
 
 const getPage = (pageUrl: string): Promise<string> =>
   new Promise((resolve, reject) => {
@@ -12,10 +13,22 @@ const getPage = (pageUrl: string): Promise<string> =>
       .on('error', reject)
   })
 
+const parseHtml = (html: string): Promise<string> =>
+  new Promise((resolve, reject) => {
+    const $ = cheerio.load(html)
+    const result = $(
+      '.col-xs-6 > .table.table-condensed.small div[style="text-align: left; margin-left: 5px;"] span[style="font-size: 18px"]'
+    )
+      .first()
+      .text()
+    resolve(result)
+  })
+
 const main = async () => {
   try {
-    // const page = await getPage(url)
-    // console.log(page)
+    const html = await getPage('https://info.ku.ac.th')
+    const data = await parseHtml(html)
+    process.stdout.write(`${data}\n`)
   } catch (err) {
     console.error(err)
   }
